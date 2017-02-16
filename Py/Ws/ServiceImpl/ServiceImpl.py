87dd30334ab70db75dff51b292931db84a707450
I-Jong Lin <ijong.lin31@gmail.coim>
2017-02-15 18:35:01 -0800

    Refactored all basic web services into Python class
    Websocket service and client webbrowser start-up hidden
    behind an Python class that can be overriden for
    different types of functionalities.  Still need to
    add the option to start for a web server to serve the UI files,
    in addition to reading directly off of the file system.
    The former is required for remote web-based UI; the latter
    is sufficient for local applications.

---
+++ Py/Ws/ServiceImpl/ServiceImpl.py
@@ -0,0 +1,66 @@
+import webbrowser
+import urllib
+import threading
+import json
+import websocket_server
+
+class ServiceImpl:
+    MESSAGE_TYPE_KEY = "MessageType"
+    MESSAGE_PAYLOAD_KEY = "MessagePayload"
+    WSJSPY_BOOTSTRAP_KEY = "WSJSPY_BOOTSTRAP"
+
+    def __init__(self, port, client_file_location, message_router):
+        ws_url = "ws://localhost:" + str(port)
+        self.ws_server = websocket_server.WebsocketServer(port)
+        self.ws_server.set_fn_message_received(self.message_received)
+        self.message_router = message_router
+        self.message_count = 0
+
+        def delay_open_ui(file_location):
+            webbrowser.open("file://" + file_location + "?"+ServiceImpl.WSJSPY_BOOTSTRAP_KEY+"="
+                            + urllib.quote(ws_url, safe=""))
+
+        self.thread_browser = threading.Timer(3, lambda: delay_open_ui(client_file_location))
+        self.thread_wsserver = threading.Timer(0, lambda: self.ws_server.run_forever())
+
+    def marshall(self, obj):
+        return json.dumps(obj)
+
+    def demarshall(self, message):
+        return json.loads(message)
+
+    def router_fallback(self, name, payload):
+        raise RuntimeError("Unrecognized message to be routed.")
+
+    def message_received(self, client, server, message):
+        self.message_count += 1
+        print("MESSAGE[{0}] -- {1}".format(self.message_count, message))
+        parsed_message = json.loads(message)
+        message_name = parsed_message[ServiceImpl.MESSAGE_TYPE_KEY]
+        message_payload =parsed_message[ServiceImpl.MESSAGE_PAYLOAD_KEY]
+        if message_name in self.message_router:
+            self.message_router[message_name](self, message_payload)
+        else:
+            self.router_fallback(message_name, message_payload)
+
+    def start(self):
+        # Note that there's a race condition between websocket server and browser
+        self.thread_wsserver.start()
+        self.thread_browser.start()
+
+    def wait_for_server_exit(self, timeout = None):
+        self.thread_wsserver.join(timeout)
+
+    def send_raw(self, message):
+        self.ws_server.send_message_to_all(message)
+
+    def shutdown(self):
+        self.ws_server.shutdown()
+
+    def send(self, message, payload):
+        self.send_raw(
+            self.marshall({
+                ServiceImpl.MESSAGE_TYPE_KEY: message,
+                ServiceImpl.MESSAGE_PAYLOAD_KEY: payload
+            })
+        )
--- Py/test/basic_client/basic_services.py
+++ Py/test/basic_client/basic_services.py
@@ -1,72 +1,35 @@
-import json
-import threading
-import urllib
-import webbrowser
 import os
+import ServiceImpl
 
-import websocket_server
+print("Starting basic client test...")
 
-print("Starting robot service...")
-
-
-def send_json_wrapped(client, server, message):
-    server.send_message(client, json.dumps(message))
-
-
-message_count = 0
-
-
-# Set up the message handlers and link back to the simulation service
-def message_received(client, server, message):
-    global message_count
-    message_count = message_count + 1
-    print("MESSAGE[{0}] -- {1}".format(message_count, message))
-    parsed_message = json.loads(message)
-    if (parsed_message["MessageType"] == "RAW" and parsed_message["MessagePayload"] == "BOOTSTRAP"):
-        send_json_wrapped(client, server, {"MessageType": "RAW",
-                                           "MessagePayload": "BOOTSTRAP_RESPONSE"
-                                           }
-                          )
-        return
-
-    if (parsed_message["MessageType"] == "RAW" and parsed_message["MessagePayload"] == "TEST_MESSAGE"):
-        send_json_wrapped(client, server, {"MessageType": "RAW",
-                                           "MessagePayload": "TEST_RESPONSE"
-                                           }
-                          )
+def raw_handler(server, message):
+    if message == "BOOTSTRAP":
+        server.send("RAW", "BOOTSTRAP_RESPONSE")
+    elif message == "TEST_MESSAGE":
+        server.send("RAW", "TEST_RESPONSE")
         server.shutdown()
-        return
+    else:
+        server.send("RAW",
+                    {
+                        "EchoMessage": message,
+                        "TransmitMessage": "Unhandled raw message!",
+                    })
 
-    # Default service
-    send_json_wrapped(client, server, {"MessageType": "RAW",
-                                       "MessagePayload": {
-                                           "EchoMessage": message,
-                                           "TransmitMessage": "HIYA!",
-                                           "MessageCount": message_count
-                                       }
-                                       }
-                      )
-
-
-print("Listening for Websockets requests on port 8080")
-port = 8080
-ws_url = "ws://localhost:" + str(port)
-ws_server = websocket_server.WebsocketServer(port)
-ws_server.set_fn_message_received(message_received)
-
-
-# Slight race condition here on start-up
-
-def delay_open_ui():
-    webbrowser.open("file://" + os.getcwd() + "/../../../Js/test/basic_client/client.html?WSJSPY_BOOTSTRAP="
-                    + urllib.quote(ws_url, safe=""))
-
+message_router = {
+    "RAW": raw_handler
+}
 
 try:
-    t = threading.Timer(3, delay_open_ui)
-    t.start()
-    ws_server.run_forever()
+    client_file_location = os.getcwd() + "/../../../Js/test/basic_client/client.html"
+    port = 8080
+
+    WsJsPyController = ServiceImpl.ServiceImpl(port, client_file_location, message_router)
+    WsJsPyController.start()
+    WsJsPyController.wait_for_server_exit()
     print("SERVER-SIDE TEST SUCCEEDS, if it gets to this point w/o exceptions.")
 except:
+    WsJsPyController.shutdown()
+    WsJsPyController.wait_for_server_exit()
     print("SERVER-SIDE TEST FAILED.")
     raise
